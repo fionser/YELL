f069e69d8216a3c8b3ed57588d000759adc29821
fionser <fionser@gmail.com>
2019-01-04 17:17:48 +0900

    Move functions in math.hpp to math.cpp

--- include/yell/utils/math.hpp
+++ include/yell/utils/math.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "yell/ops.hpp"
 #include <cmath>
+namespace yell {
 namespace math {
 // compute a^{-1} mod prime
 // i.e., a^{prime - 2}
@@ -25,28 +26,8 @@ inline T round(F a)
   return a >= 0 ? (T)(a + 0.5f) : (T) (a - 0.5f);
 }
 
-inline bool is_same(double a, double b) 
-{
-  if (std::abs(a - b) < 1e-8)
-    return true;
-  return std::abs(std::abs((a - b)) / std::max(a, b)) < 1e-8;
-}
-
 /* Reverse 32-bit */
-uint32_t reverse_bits(uint32_t operand) 
-{
-  operand = (((operand & 0xaaaaaaaa) >> 1) | ((operand & 0x55555555) << 1));
-  operand = (((operand & 0xcccccccc) >> 2) | ((operand & 0x33333333) << 2));
-  operand = (((operand & 0xf0f0f0f0) >> 4) | ((operand & 0x0f0f0f0f) << 4));
-  operand = (((operand & 0xff00ff00) >> 8) | ((operand & 0x00ff00ff) << 8));
-  return((operand >> 16) | (operand << 16));
-}
-
-/* The operand is less than 32 */
-uint32_t reverse_bits(uint32_t operand, int32_t bit_count)  
-{
-  assert(bit_count < 32);
-  return (uint32_t) (((uint64_t) reverse_bits(operand)) >> (32 - bit_count));
-}
-} // namespace math
+uint32_t reverse_bits(uint32_t operand);
+uint32_t reverse_bits(uint32_t operand, int32_t bit_count);
+}} // namespace yell::math
 
