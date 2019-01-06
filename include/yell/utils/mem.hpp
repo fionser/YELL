0897f6e7fc6d078732d4c44a6f7113b830df668f
riku <fionser@gmail.com>
2019-01-06 19:51:27 +0900

    Hide the detail of memory allocation.

--- include/yell/utils/mem.hpp
+++ include/yell/utils/mem.hpp
@@ -1,32 +1,6 @@
 #pragma once
-#if 0
-#include <gperftools/tcmalloc.h>
-#include <memory>
-#include <cassert>
-
-void * mem_alloc(size_t bytes) {
-  void *p = tc_malloc(bytes);
-  assert(p);
-  return p;
-}
-
-void mem_free(void *p) {
-  if (p) tc_free(p);
-}
-
-#else
-#include <memory>
-#include <stdlib.h>
-#include <cassert>
-
-void * mem_alloc(size_t bytes) {
-  void* res;
-  const int failed = posix_memalign(&res, 32, bytes);
-  if (failed) res = 0;
-  return res;
-}
-
-void mem_free(void *p) {
-  if (p) free(p);
-}
-#endif
+#include <cstddef>
+namespace yell {
+void * mem_alloc(size_t bytes);
+void mem_free(void *p);
+} // namespace yell
---
+++ lib/mem.cpp
@@ -0,0 +1,25 @@
+#include "yell/utils/mem.hpp"
+#include <stdlib.h>
+namespace yell {
+
+#ifndef YELL_NTT_AVX
+
+void * mem_alloc(size_t bytes) {
+  void* res = malloc(bytes);
+  return res;
+}
+#else
+
+void * mem_alloc(size_t bytes) {
+  void* res;
+  const int failed = posix_memalign(&res, 32, bytes);
+  if (failed) res = 0;
+  return res;
+}
+#endif
+
+void mem_free(void *p) {
+  if (p) free(p);
+}
+
+} // namespace yell
