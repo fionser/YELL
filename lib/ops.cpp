e3f3f028d88df37c7cd5cf8abc333c30bc2425a8
riku <fionser@gmail.com>
2019-01-19 16:19:47 +0900

    Refactoring avx_ntt codes. :xa

--- lib/ops.cpp
+++ lib/ops.cpp
@@ -7,16 +7,15 @@ void barret_reduction(params::gt_value_type *x, size_t cm)
   if (!x) return;
   using T  = params::value_type;
   using gT = params::gt_value_type;
-  using ST = params::signed_type;
 
   const gT _x = *x;
-  const T pm = params::Pn[cm];
+  const gT pm = params::Pn[cm];
   constexpr size_t shift = params::kModulusRepresentationBitsize;
   constexpr size_t delta = shift - params::kModulusBitsize;
-  gT q = ((gT) pm * (_x >> shift)) + (_x << delta) ;
+  gT q = (pm * (_x >> shift)) + (_x << delta) ;
 
   const T p[2] = {params::P[cm], 0};
-  T r = (T) _x - ((T) (q >> shift)) * p[0];
+  T r = (T) _x - ((T) (q >> shift)) * p[0]; //! implicit mod 2^w
   r -= p[r < p[0]]; //! if (r >= params::P[cm]) r -= params::P[cm];
   *x = (gT) r;
 } 
@@ -26,4 +25,5 @@ yell::params::value_type shoupify(yell::params::value_type x, size_t cm)
   using gT = params::gt_value_type;
   return ((gT) x << params::kModulusRepresentationBitsize) / params::P[cm];
 }
+
 }} // namespace yell::ops
