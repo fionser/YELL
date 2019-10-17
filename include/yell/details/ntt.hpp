b3949f1febc659def28b3b650777f0b8f431510b
居侯 <juhou.lwj@alibaba-inc.com>
2019-10-17 15:17:34 +0800

    2^{-1} mod p trick to save the n^{-1} step in invNTT transformation.

--- include/yell/details/ntt.hpp
+++ include/yell/details/ntt.hpp
@@ -56,13 +56,11 @@ void ntt<degree>::ntt_precomputed::init(size_t cm) {
   // phi^(2*degree) = 1 -> temp * phi^(degree-1) = phi^(-1)
   const T invphi = mulmod(temp, phiTbl[degree - 1], cm);
 
-  // Computation of the inverse of degree using the inverse of kMaxPolyDegree
-  invDegree = mulmod(params::invkMaxPolyDegree[cm], (uint64_t)(params::kMaxPolyDegree/degree), cm);
-  shoupInvDegree = ops::shoupify(invDegree, cm);
-
+  const T prime = yell::params::P[cm];
+  auto div_2_mod_p = [](T v, T p) { return v & 1 ?  (v + p) >> 1 : v >> 1; };
   temp = 1;
   for (unsigned int i = 0 ; i < degree; i++) {
-    invphiTbl[i] = temp;
+    invphiTbl[i] = div_2_mod_p(temp, prime);
     shoupInvphiTbl[i] = ops::shoupify(invphiTbl[i], cm);
     mulmod.compute(temp, invphi, cm);
   }
@@ -80,9 +78,6 @@ void ntt<degree>::ntt_precomputed::init(size_t cm) {
 
   permute<degree>::compute(tmp, shoupInvphiTbl);
   std::memcpy(shoupInvphiTbl, tmp, sizeof(shoupInvphiTbl));
-
-  invphiInvDegree = mulmod(invDegree, invphiTbl[1], cm);
-  shoupInvphiInvDegree = ops::shoupify(invphiInvDegree, cm);
 }
 
 template<size_t degree> const typename ntt<degree>::ntt_precomputed* 
@@ -149,29 +144,10 @@ void ntt<degree>::backward(T *op, size_t cm)
   const auto tbl = init_table(cm);
   const T *invphiTbl = tbl->invphiTbl;
   const T *shoupInvphiTbl = tbl->shoupInvphiTbl;
-
   negacylic_backward_lazy(op, degree, invphiTbl, shoupInvphiTbl, params::P[cm]);
-  //! merge the last layer butterfly with n^-1 step.
-  ops::mulmod_shoup mulmod;
-  const T invDegree = tbl->invDegree;
-  const T shoupInvDegree = tbl->shoupInvDegree;
-  const T invphiInvDegree = tbl->invphiInvDegree;
-  const T shoupInvphiInvDegree = tbl->shoupInvphiInvDegree;
-  const T w = invphiTbl[1];
-  const T shoupw = shoupInvphiTbl[1];
-  const T _2p = 2 * yell::params::P[cm];
-
-  auto x0 = &op[0];
-  auto x_half = &op[degree >> 1u];
-  auto x1 = x_half;
-  while (x0 != x_half) {
-    auto u = *x0 + *x1; // lazy reduction
-    auto v = *x0 + _2p - *x1; // i.e., x0 - x1
-    mulmod.compute(u, invDegree, shoupInvDegree, cm);
-    mulmod.compute(v, invphiInvDegree, shoupInvphiInvDegree, cm);
-    *x0++ = u;
-    *x1++ = v;
-  }
+  const T p = params::P[cm];
+  T mod_table[2]{p, 0};
+  std::transform(op, op + degree, op, [&mod_table, p](T v) { return v - mod_table[v < p]; });
 #ifndef NDEBUG
   for (size_t d = 0; d < degree; ++d)
     assert(op[d] < yell::params::P[cm]);
--- lib/ntt.cpp
+++ lib/ntt.cpp
@@ -18,18 +18,8 @@ struct ntt_loop_body {
   using gt_value_type = params::gt_value_type;
   const size_t bit_width = params::kModulusRepresentationBitsize;
   const value_type p, _2p;
-#ifdef YELL_USE_AVX_NTT //! see yell/defines.h
-  __m256i avx_2p, avx_p, avx_80, avx_2pcomp; 
-#endif
 
-  explicit ntt_loop_body(value_type const p) : p(p), _2p(p * 2) {
-#ifdef YELL_USE_AVX_NTT
-    avx_2p = _mm256_set1_epi32(p << 1);
-    avx_p  = _mm256_set1_epi32(p);
-    avx_80 = _mm256_set1_epi32(0x80000000);
-    avx_2pcomp = _mm256_set1_epi32((2*p) - 0x80000000 - 1);
-#endif
-  }
+  explicit ntt_loop_body(value_type const p) : p(p), _2p(p * 2) { }
 
   //! if (cond is true) return a else return b
   inline value_type const_time_select(value_type a, value_type b, bool cond) const {
@@ -37,6 +27,7 @@ struct ntt_loop_body {
       value_type x = a ^ b;
       return (x & c) ^ b;
   }
+
   //! x'0 = x0 + x1 mod p
   //! x'1 = w * (x0 - x1) mod p
   //! Require: 0 < x0, x1 < 2 * p
@@ -54,7 +45,8 @@ struct ntt_loop_body {
     value_type t1 = u0 - u1 + _2p;
     value_type q = ((gt_value_type) t1 * wprime) >> bit_width;
 
-    *x0 = t0;
+    //! w is in the div_2_mod_p form, so we need to handle x0 here.
+    *x0 = const_time_select((t0 + p) >> 1, t0 >> 1, t0 & 1); 
     *x1 = t1 * w - q * p;
   }
 
@@ -76,66 +68,8 @@ struct ntt_loop_body {
     *x0 = u0 + t;
     *x1 = u0 - t + _2p;
   }
-
-#ifdef YELL_USE_AVX_NTT
-  inline void avx_gs_butterfly(value_type* x0, 
-                               value_type* x1, 
-                               __m256i const avx_w,
-                               __m256i const avx_wprime) const
-  {
-    __m256i avx_u0, avx_u1;
-    __m256i avx_t0, avx_t1, avx_t2;
-    __m256i avx_q, avx_cmp;
-
-    avx_u0 = _mm256_load_si256((__m256i const*) x0);
-    avx_u1 = _mm256_load_si256((__m256i const*) x1);
-
-    //! t1 = u0 - u1 + 2p
-    avx_t1 = _mm256_add_epi32(avx_2p, _mm256_sub_epi32(avx_u0, avx_u1));
-    //! q = high 32 bit of (t1 * wprime)
-    avx_q = avx_mm256_mul32_hi(avx_t1, avx_wprime);
-    avx_t2 = _mm256_sub_epi32(_mm256_mullo_epi32(avx_t1, avx_w),
-                              _mm256_mullo_epi32(avx_q, avx_p));
-
-    avx_t0 = _mm256_add_epi32(avx_u0, avx_u1);
-    avx_cmp = _mm256_cmpgt_epi32(_mm256_sub_epi32(avx_t0, avx_80), avx_2pcomp);
-    avx_t0 = _mm256_sub_epi32(avx_t0, _mm256_and_si256(avx_cmp, avx_2p));
-
-    _mm256_store_si256((__m256i*) x0, avx_t0);
-    _mm256_store_si256((__m256i*) x1, avx_t2);
-  }
-
-  inline void avx_ct_butterfly(value_type* x0, 
-                               value_type* x1, 
-                               __m256i const avx_w,
-                               __m256i const avx_wprime) const
-  {
-    __m256i avx_u0, avx_u1;
-    __m256i avx_t0, avx_t1;
-    __m256i avx_q, avx_cmp;
-
-    avx_u0 = _mm256_load_si256((__m256i const*) x0);
-    avx_u1 = _mm256_load_si256((__m256i const*) x1);
-
-    //! q = high 32bit of (u1 * wprime)
-    avx_q = avx_mm256_mul32_hi(avx_u1, avx_wprime);
-    //! u1 * w - q * p
-    avx_t1 = _mm256_sub_epi32(_mm256_mullo_epi32(avx_u1, avx_w),
-                              _mm256_mullo_epi32(avx_q, avx_p));
-
-    avx_cmp = _mm256_cmpgt_epi32(_mm256_sub_epi32(avx_u0, avx_80), avx_2pcomp);
-    avx_t0 = _mm256_sub_epi32(avx_u0, _mm256_and_si256(avx_cmp, avx_2p));
-
-    avx_u0 = _mm256_add_epi32(avx_t0, avx_t1);
-    avx_u1 = _mm256_add_epi32(_mm256_sub_epi32(avx_t0, avx_t1), avx_2p);
-
-    _mm256_store_si256((__m256i*) x0, avx_u0);
-    _mm256_store_si256((__m256i*) x1, avx_u1);
-  }
-#endif // YELL_USE_AVX_NTT
 };
 
-#ifndef YELL_USE_AVX_NTT
 //! x in given in standard order.
 //! wtab is the powers of 2*degree primitive root in the bit-reversed order.
 //! e.g., w^{2 * degree} = 1 \bmod p
@@ -173,7 +107,7 @@ void negacylic_backward_lazy(
 {
   ntt_loop_body body(p);
   size_t t = 1;
-  for (size_t m = degree; m > 2; m >>= 1u) { //! 'm > 2' to skip the last layer.
+  for (size_t m = degree; m > 1; m >>= 1u) {
     const size_t h = m >> 1u;
     const params::value_type *w = &wtab[h];
     const params::value_type *wshoup = &wtab_shoup[h];
@@ -187,80 +121,4 @@ void negacylic_backward_lazy(
   }
   //! x[0 .. degree) stay in the range [0, 2p)
 }
-#else // YELL_USE_AVX_NTT
-
-void negacylic_forward_lazy(
-  params::value_type *x, 
-  const size_t degree,
-  const params::value_type *wtab,
-  const params::value_type *wtab_shoup,
-  const params::value_type p)
-{
-  ntt_loop_body body(p);
-  __m256i avx_w, avx_wprime;
-  size_t t = degree;
-  for (size_t m = 1; m < degree; m <<= 1) {
-    t >>= 1u;
-    const params::value_type *w = &wtab[m];
-    const params::value_type *wshoup = &wtab_shoup[m];
-    if (t > 7) {
-      for (size_t i = 0; i != m; ++i) {
-        auto x0 = &x[2 * i * t];
-        auto x1 = x0 + t;
-        avx_w = _mm256_set1_epi32(w[i]);
-        avx_wprime = _mm256_set1_epi32(wshoup[i]);
-        for (size_t j = 0; j != t; j += 8, x0 += 8, x1 += 8)
-          body.avx_ct_butterfly(x0, x1, avx_w, avx_wprime);
-      }
-    } else { //! last two layers
-      for (size_t i = 0; i != m; ++i) {
-        auto x0 = &x[2 * i * t];
-        auto x1 = x0 + t;
-        for (size_t j = 0; j != t; ++j)
-          body.ct_butterfly(x0++, x1++, w[i], wshoup[i]);
-      }
-    }
-  }
-  //! x[0 .. degree) stay in the range [0, 4p)
-}
-
-void negacylic_backward_lazy(
-  params::value_type *x, 
-  const size_t degree,
-  const params::value_type *wtab,
-  const params::value_type *wtab_shoup,
-  const params::value_type p)
-{
-  ntt_loop_body body(p);
-  size_t t = 1;
-  __m256i avx_w, avx_wprime;
-  for (size_t m = degree; m > 2; m >>= 1u) { //! 'm > 2' to skip the last layer.
-    const size_t h = m >> 1u;
-    size_t j1 = 0;
-    const params::value_type *w = &wtab[h];
-    const params::value_type *wshoup = &wtab_shoup[h];
-    if (t > 7) { 
-      for (size_t i = 0; i < h; ++i) {
-        auto x0 = &x[j1];
-        auto x1 = x0 + t;
-        avx_w = _mm256_set1_epi32(w[i]);
-        avx_wprime = _mm256_set1_epi32(wshoup[i]);
-        for (size_t j = 0; j < t; j += 8, x0 += 8, x1 += 8)
-          body.avx_gs_butterfly(x0, x1, avx_w, avx_wprime);
-        j1 = j1 + (t << 1u);
-      }
-    } else { //! first two layers
-      for (size_t i = 0; i < h; ++i) {
-        auto x0 = &x[j1];
-        auto x1 = x0 + t;
-        for (size_t j = 0; j < t; ++j)
-          body.gs_butterfly(x0++, x1++, w[i], wshoup[i]);
-        j1 = j1 + (t << 1u);
-      }
-    }
-    t <<= 1u;
-  }
-  //! x[0 .. degree) stay in the range [0, 2p)
-}
-#endif // YELL_USE_AVX_NTT
 } // namespace yell
